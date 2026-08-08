85a036e33c965e064a8ad8ae36415e831758293e
Umesh Chandra <umeshchandra01111999@gmail.com>
2026-08-08 17:35:10 +0530

    actions

--- Modules_VPC/main.tf
+++ Modules_VPC/main.tf
@@ -2,6 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
-  
   tags = local.vpc_final_tags
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = local.igw_final_tags
 }
--- module_test_VPC/vpc.tf
+++ module_test_VPC/vpc.tf
@@ -1,3 +1,5 @@
 module "vpc" {
     source = "git::https://github.com/Umesh16082019/Terraform.git//Modules_VPC?ref=main"
+    project = var.project
+    environment = var.environment
 }
