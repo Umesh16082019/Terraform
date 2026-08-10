bdcfaa72515f5327ff42e169952faf5750793a09
Umesh Chandra <umeshchandra01111999@gmail.com>
2026-08-10 07:47:24 +0530

    actions

--- module_test_VPC/vpc.tf
+++ module_test_VPC/vpc.tf
@@ -2,4 +2,5 @@ module "vpc" {
     source = "git::https://github.com/Umesh16082019/Terraform.git//Modules_VPC?ref=main"
     project = var.project
     environment = var.environment
+    is_peering_required = true
 }
