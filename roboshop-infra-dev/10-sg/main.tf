91bd96e09f7cb707b9c277525209579513fa11fb
Umesh Chandra <umeshchandra01111999@gmail.com>
2026-08-13 06:50:11 +0530

    actions

--- roboshop-infra-dev/10-sg/main.tf
+++ roboshop-infra-dev/10-sg/main.tf
@@ -1,1 +1,7 @@
-module "aws_security_group"
+module "sg" {
+    source = "git::https://github.com/Umesh16082019/Terraform.git//Modules_SG?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_name="MongoDB"
+    vpc_id=local.vpc_id
+}
